81e1d0dd67cd4992b8c14c4ce44bab470ff5bb3e
AparnaNallamolu <78528849+AparnaNallamolu@users.noreply.github.com>
2025-04-07 13:03:33 +0530

    Create Module 5: Files, Exceptions, and Errors in Python Assignment.py

---
+++ Module 5: Files, Exceptions, and Errors in Python Assignment.py
@@ -0,0 +1,39 @@
+'''Task 1: Read a File and Handle Errors 
+Problem Statement:  Write a Python program that:
+1.   Opens and reads a text file named sample.txt.
+2.   Prints its content line by line.
+3.   Handles errors gracefully if the file does not exist.
+'''
+
+
+file1 = open('sample.txt', 'r')
+reading_file = file1.read()
+print(reading_file)
+file1.close()
+
+'''
+Task 2: Write and Append Data to a File
+ 
+Problem Statement: Write a Python program that:
+1.   Takes user input and writes it to a file named output.txt.
+2.   Appends additional data to the same file.
+3.   Reads and displays the final content of the file.
+'''
+
+file = open('output.txt', 'r+')
+write_file = input('Enter text to write to the file:')
+writing = file.write(write_file + '\n')
+print('Data successfully written to output.txt')
+file.close()
+
+
+file = open('output.txt', 'a')
+append_data = input('Enter additional text to append:')
+appending = file.writelines(append_data  + '\n')
+print('Data Successfully appended')
+file.close()
+
+file = open('output.txt', 'r')
+final_data = file.read()
+print('Final content of output.txt:\n',final_data)
+file.close()
